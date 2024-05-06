acdd323304e7277f405a8d8bf72f037cd567afa3
SuperStriker-2 <masaldanaryan@gmail.com>
2024-05-05 23:51:50 -0400

    stuff

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const loader = new GLTFLoader();
+
+loader.load( 'static/out.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+camera.position.z = 2;
+camera.position.x=-2;
+camera.rotation.z=0.001;
+
+
+function animate() {
+	requestAnimationFrame( animate );
+  camera.rotation.y+=-0.001;
+	renderer.render( scene, camera );
+}
+animate();
