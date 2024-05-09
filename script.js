66def77b40b9023202fbbbcc477ae251a2872f1b
SuperStriker-2 <masaldanaryan@gmail.com>
2024-05-08 23:09:07 -0400

    slay queen
    ....

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { BloomPass } from 'three/addons/postprocessing/BloomPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: myCanvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// document.body.appendChild( renderer.domElement );
+
+var thing;
 
 const loader = new GLTFLoader();
 
 loader.load( 'static/out.glb', function ( gltf ) {
 
   const model=gltf.scene
+  thing=model;
 	scene.add( model );
 
 }, undefined, function ( error ) {
@@ -24,23 +27,29 @@ loader.load( 'static/out.glb', function ( gltf ) {
 	console.error( error );
 
 } );
-camera.position.z = 1.5;
-camera.position.x=-1.5;
-camera.rotation.y=-0.1;
+
+camera.position.z+=1.6;
+camera.position.y+=0.15;
 
 const composer = new EffectComposer( renderer );
 
 const renderPass = new RenderPass( scene, camera );
 composer.addPass( renderPass );
 
-const bloomPass = new BloomPass(0.1);
-composer.addPass( bloomPass );
+const ubloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth,window.innerHeight),
+  0.3,
+  0.2,
+  0.1
+);
+composer.addPass( ubloomPass );
 
 const outputPass = new OutputPass();
 composer.addPass( outputPass );
 
 function animate() {
 	requestAnimationFrame( animate );
+  thing.rotation.y+=0.0005;
 	composer.render();
 }
 animate();
