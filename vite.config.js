100c80643a69279b9125a076c1a1f9a991339644
AyushB08 <ayushbheemaiah@gmail.com>
2024-05-21 17:56:40 -0700

    dependency fix v1

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'three': path.resolve(__dirname, 'node_modules/three')
+    resolve: {
+      alias: {
+        'three': path.resolve(__dirname, 'node_modules/three/build/three.module.js')
+      }
+    },
+    optimizeDeps: {
+      include: [
+        'three/examples/jsm/loaders/GLTFLoader',
+        'three/examples/jsm/postprocessing/EffectComposer',
+        'three/examples/jsm/postprocessing/RenderPass',
+        'three/examples/jsm/postprocessing/UnrealBloomPass',
+        'three/examples/jsm/postprocessing/OutputPass'
+      ]
     }
-  },
-  optimizeDeps: {
-    include: ['three']
-  }
-});
+  });
+  
