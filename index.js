e10fdaef12386ab5936b2cca6fb519e090776faf
SuperStriker-2 <masaldanaryan@gmail.com>
2024-05-31 23:36:21 -0400

    quik fix?

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from './node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js';
-import { OutputPass } from './node_modules/three/examples/jsm/postprocessing/OutputPass.js';
-
-
-const canvinfo = document.getElementById('myCanvas').getBoundingClientRect();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, canvinfo.width / canvinfo.height, 0.1, 100);
-
-const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: myCanvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-let thing;
-
-const loader = new GLTFLoader();
-loader.load('static/out.glb', function (gltf) {
-  const model = gltf.scene;
-  thing = model;
-  scene.add(model);
-}, undefined, function (error) {
-  console.error(error);
-});
-
-let scrollPercent = ((document.documentElement.scrollTop || document.body.scrollTop) /
-  ((document.documentElement.scrollHeight || document.body.scrollHeight) -
-    document.documentElement.clientHeight));
-
-camera.position.z = 1.6 * (1 - scrollPercent);
-camera.position.y += 0.15;
-
-const composer = new EffectComposer(renderer);
-const renderPass = new RenderPass(scene, camera);
-composer.addPass(renderPass);
-
-const ubloomPass = new UnrealBloomPass(
-  new THREE.Vector2(canvinfo.width, canvinfo.height),
-  0.3,
-  0.2,
-  0.1
-);
-composer.addPass(ubloomPass);
-
-const outputPass = new OutputPass();
-composer.addPass(outputPass);
-
-let newValue;
-
-document.body.onscroll = () => {
-  scrollPercent =
-    ((document.documentElement.scrollTop || document.body.scrollTop) /
-      ((document.documentElement.scrollHeight || document.body.scrollHeight) -
-        document.documentElement.clientHeight));
-  camera.position.z = 1.6 * (1 - scrollPercent);
-  newValue = window.pageYOffset;
-};
-
-window.addEventListener('resize', onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-}
-
-function render() {
-  composer.render();
-}
+// import * as THREE from './node_modules/three/build/three.module.js';
+// import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+// import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
+// import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
+// import { UnrealBloomPass } from './node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js';
+// import { OutputPass } from './node_modules/three/examples/jsm/postprocessing/OutputPass.js';
+//
+//
+// const canvinfo = document.getElementById('myCanvas').getBoundingClientRect();
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75, canvinfo.width / canvinfo.height, 0.1, 100);
+//
+// const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: myCanvas });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+//
+// let thing;
+//
+// const loader = new GLTFLoader();
+// loader.load('static/out.glb', function (gltf) {
+//   const model = gltf.scene;
+//   thing = model;
+//   scene.add(model);
+// }, undefined, function (error) {
+//   console.error(error);
+// });
+//
+// let scrollPercent = ((document.documentElement.scrollTop || document.body.scrollTop) /
+//   ((document.documentElement.scrollHeight || document.body.scrollHeight) -
+//     document.documentElement.clientHeight));
+//
+// camera.position.z = 1.6 * (1 - scrollPercent);
+// camera.position.y += 0.15;
+//
+// const composer = new EffectComposer(renderer);
+// const renderPass = new RenderPass(scene, camera);
+// composer.addPass(renderPass);
+//
+// const ubloomPass = new UnrealBloomPass(
+//   new THREE.Vector2(canvinfo.width, canvinfo.height),
+//   0.3,
+//   0.2,
+//   0.1
+// );
+// composer.addPass(ubloomPass);
+//
+// const outputPass = new OutputPass();
+// composer.addPass(outputPass);
+//
+// let newValue;
+//
+// document.body.onscroll = () => {
+//   scrollPercent =
+//     ((document.documentElement.scrollTop || document.body.scrollTop) /
+//       ((document.documentElement.scrollHeight || document.body.scrollHeight) -
+//         document.documentElement.clientHeight));
+//   camera.position.z = 1.6 * (1 - scrollPercent);
+//   newValue = window.pageYOffset;
+// };
+//
+// window.addEventListener('resize', onWindowResize, false);
+// function onWindowResize() {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+//   render();
+// }
+//
+// function render() {
+//   composer.render();
+// }
 
 function links() {
   let l = document.getElementById("navlinks");
@@ -107,11 +107,11 @@ let observer = new IntersectionObserver(handleIntersection);
 let target = document.getElementById("service-list");
 observer.observe(target);
 
-function animate() {
-  requestAnimationFrame(animate);
-  if (thing) { // Check if thing is defined
-    thing.rotation.y += 0.0005;
-  }
-  composer.render();
-}
-animate();
+// function animate() {
+//   requestAnimationFrame(animate);
+//   if (thing) { // Check if thing is defined
+//     thing.rotation.y += 0.0005;
+//   }
+//   composer.render();
+// }
+// animate();
--- spotlight_script.js
+++ spotlight_script.js
@@ -1,82 +1,82 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from './node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js';
-import { OutputPass } from './node_modules/three/examples/jsm/postprocessing/OutputPass.js';
-
-
-const canvinfo = document.getElementById('myCanvas').getBoundingClientRect();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, canvinfo.width / canvinfo.height , 0.1, 100 );
-
-const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: myCanvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-let thing;
-
-const loader = new GLTFLoader();
-
-loader.load( 'static/neuron.glb', function ( gltf ) {
-
-  const model=gltf.scene
-  thing=model;
-	scene.add( model );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
-
-let scrollPercent=((document.documentElement.scrollTop || document.body.scrollTop) /
-    ((document.documentElement.scrollHeight ||
-        document.body.scrollHeight) -
-        document.documentElement.clientHeight));
-
-// camera.position.z=1.6*(1-scrollPercent);
-camera.position.y+=0.15;
-camera.position.z=5;
-
-const composer = new EffectComposer( renderer );
-
-const renderPass = new RenderPass( scene, camera );
-composer.addPass( renderPass );
-
-const ubloomPass = new UnrealBloomPass(
-  new THREE.Vector2(canvinfo.width,canvinfo.height),
-  0.45,
-  1,
-  0.5
-);
-composer.addPass( ubloomPass );
-
-const outputPass = new OutputPass();
-composer.addPass( outputPass );
-
-let newValue;
-
-// document.body.onscroll = () => {
-//     //calculate the current scroll progress as a percentage
-//     scrollPercent =
-//         ((document.documentElement.scrollTop || document.body.scrollTop) /
-//             ((document.documentElement.scrollHeight ||
-//                 document.body.scrollHeight) -
-//                 document.documentElement.clientHeight));
-//       camera.position.z=1.6*(1-scrollPercent);
-//       newValue = window.pageYOffset;
+// import * as THREE from './node_modules/three/build/three.module.js';
+// import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+// import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
+// import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
+// import { UnrealBloomPass } from './node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js';
+// import { OutputPass } from './node_modules/three/examples/jsm/postprocessing/OutputPass.js';
 //
+//
+// const canvinfo = document.getElementById('myCanvas').getBoundingClientRect();
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera( 75, canvinfo.width / canvinfo.height , 0.1, 100 );
+//
+// const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: myCanvas});
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// // document.body.appendChild( renderer.domElement );
+//
+// let thing;
+//
+// const loader = new GLTFLoader();
+//
+// loader.load( 'static/neuron.glb', function ( gltf ) {
+//
+//   const model=gltf.scene
+//   thing=model;
+// 	scene.add( model );
+//
+// }, undefined, function ( error ) {
+//
+// 	console.error( error );
+//
+// } );
+//
+// let scrollPercent=((document.documentElement.scrollTop || document.body.scrollTop) /
+//     ((document.documentElement.scrollHeight ||
+//         document.body.scrollHeight) -
+//         document.documentElement.clientHeight));
+//
+// // camera.position.z=1.6*(1-scrollPercent);
+// camera.position.y+=0.15;
+// camera.position.z=5;
+//
+// const composer = new EffectComposer( renderer );
+//
+// const renderPass = new RenderPass( scene, camera );
+// composer.addPass( renderPass );
+//
+// const ubloomPass = new UnrealBloomPass(
+//   new THREE.Vector2(canvinfo.width,canvinfo.height),
+//   0.45,
+//   1,
+//   0.5
+// );
+// composer.addPass( ubloomPass );
+//
+// const outputPass = new OutputPass();
+// composer.addPass( outputPass );
+//
+// let newValue;
+//
+// // document.body.onscroll = () => {
+// //     //calculate the current scroll progress as a percentage
+// //     scrollPercent =
+// //         ((document.documentElement.scrollTop || document.body.scrollTop) /
+// //             ((document.documentElement.scrollHeight ||
+// //                 document.body.scrollHeight) -
+// //                 document.documentElement.clientHeight));
+// //       camera.position.z=1.6*(1-scrollPercent);
+// //       newValue = window.pageYOffset;
+// //
+// // }
+// document.getElementById('myCanvas').style.opacity="0.4";
+//
+// window.addEventListener('resize', onWindowResize, false)
+// function onWindowResize() {
+//     camera.aspect = window.innerWidth / window.innerHeight
+//     camera.updateProjectionMatrix()
+//     renderer.setSize(window.innerWidth, window.innerHeight)
+//     render()
 // }
-document.getElementById('myCanvas').style.opacity="0.4";
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
 
 function links() {
   let l = document.getElementById("navlinks");
@@ -97,30 +97,30 @@ function links() {
 }
 let listen = document.getElementById("brain");
 listen.addEventListener("click",links);
-//
-// function handleIntersection(entries) {
-//   entries.map((entry) => {
-//     if (entry.isIntersecting) {
-//       for (var i=0;i<target.children.length;i++){
-//         target.children[i].classList.add('slideIn');
-//       }
-//     }
-//     else {
-//       for (var i=0;i<target.children.length;i++){
-//         target.children[i].classList.remove('slideIn');
-//       }
-//     }
-//   });
-// }
-//
-// let observer = new IntersectionObserver(handleIntersection);
-// let target = document.getElementById("service-list");
-// observer.observe(target);
-//
 
-function animate() {
-	requestAnimationFrame( animate );
-  thing.rotation.y+=0.0005;
-	composer.render();
+function handleIntersection(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      for (var i=0;i<target.children.length;i++){
+        target.children[i].classList.add('slideIn');
+      }
+    }
+    else {
+      for (var i=0;i<target.children.length;i++){
+        target.children[i].classList.remove('slideIn');
+      }
+    }
+  });
 }
-animate();
+
+let observer = new IntersectionObserver(handleIntersection);
+let target = document.getElementById("service-list");
+observer.observe(target);
+
+
+// function animate() {
+// 	requestAnimationFrame( animate );
+//   thing.rotation.y+=0.0005;
+// 	composer.render();
+// }
+// animate();
